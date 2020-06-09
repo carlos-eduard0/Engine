aa06f866b685cdee456aff65ab7a32e89d99c654
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-06-08 21:04:53 -0400

    up

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://api.engine-company.com:8443/',
     // baseURL: 'http://localhost:7070',
 });
 
