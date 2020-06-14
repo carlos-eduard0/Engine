4d5ed02aa4d13c046cc6d653342ae0ba45b441bd
EKaynan <erickaynan11@gmail.com>
2020-06-14 04:24:30 -0300

    arrumando trocar senha

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:7070' || 'https://api.engine-company.com:8443/',
+    baseURL: 'https://api.engine-company.com:8443/',
     // baseURL: 'http://localhost:7070',
 });
 
