5cc1000454edb25c498789a7d76786dd043a0250
Ronan Laurindo Flôr <ronanlf12@gmail.com>
2020-06-15 17:47:42 -0400

    nome da empresa

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -15,7 +15,7 @@ function Painel(){
     const empresa_id = cookies.get('id');
 
     useEffect(() => {
-        api.get('/empresa', {
+        api.get('/header', {
             headers: {
                 Authorization: empresa_id,
             }
