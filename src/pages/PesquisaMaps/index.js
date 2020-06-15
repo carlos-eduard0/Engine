c1c470675baa4c08a080bb2b12aaa935aafa626c
carlos-eduard0 <eduard.mascarenhas.ce@gmail.com>
2020-06-15 16:21:33 -0400

    up

--- src/pages/PesquisaMaps/index.js
+++ src/pages/PesquisaMaps/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 import usePlacesAutocomplete from "use-places-autocomplete";
 import {
   geocodeByAddress,
-  getLatLng
+  getLatLng,
 } from "react-places-autocomplete";
 import {
   Combobox,
@@ -15,24 +15,37 @@ import {
 
 import "@reach/combobox/styles.css";
 
-const [InitialPosition ,setInitialPosition] = useState<[number, number]>([0, 0]);
-
 const PlacesAutoComplete = ({ onGetLatLng }) => {
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-      setInitialPosition([latitude, longitude]);
-    })
-  }, [])
-  
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
   const {
     ready,
     value,
     suggestions: { status, data },
     setValue,
     clearSuggestions,
-  } = usePlacesAutocomplete();
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: new window.google.maps.LatLng(latitude, longitude),
+      radius: 15000,
+    }
+  });
 
   const [coordinates, setCoordinates] = React.useState({
     lat: null,
@@ -50,29 +63,18 @@ const PlacesAutoComplete = ({ onGetLatLng }) => {
     setValue(val, false);
     onGetLatLng([latLng, results[0].address_components]);
     setCoordinates(latLng);
-
-    console.log(results);
   };
   return (
     <Combobox onSelect={handleSelect} aria-labelledby="demo">
-      <ComboboxInput value={value} onChange={handleInput} disabled={!ready} className="inputEnd" query={{
-        language:'pt-br',
-        location: `${InitialPosition}`,
-        radius: '150000',
-        components: 'country:mx',
-        strinctbounds: true,
-      }} />
+      <ComboboxInput value={value} onChange={handleInput} disabled={!ready} className="inputEnd" />
       <ComboboxPopover>
         <ComboboxList>
 
           {status === "OK" &&
             data.map(({ id, description }) => (
-              <>
                 <ComboboxOption key={id} value={description} style={{ paddingTop: 20, paddingBottom: 20 }} >
                   📍" <ComboboxOptionText />
                 </ComboboxOption>
-
-              </>
             ))}
 
         </ComboboxList>
