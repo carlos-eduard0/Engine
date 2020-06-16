751c537187ee8c5ccbbd25f6f0179e14fe6105e0
EKaynan <erickaynan11@gmail.com>
2020-06-15 22:03:39 -0300

    redirecionando dps do login

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import auth from './services/auth'; 
+// import auth from './services/auth'; 
 import Cookies from 'universal-cookie'; 
 
 import Main from './pages/main';
@@ -10,40 +10,41 @@ import Login from './pages/login';
 import RedefinirSenha from './pages/redefinirSenha';
 import CodigoSenha from './pages/codigoSenha';
 
-const cookies = new Cookies();
-
-cookies.set('lang', 'pt-br', {path:'/'});
-
-const check = () => {
-    var user = cookies.get('id');
-
-    if(user){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-const PrivateRoute = ({ component: Component, ... rest}) => ( // Cria uma rota privada
-    <Route //Essa rota é uma rota normal, que renderiza um componente a ser informado dps...
-        {... rest}
-        render={props =>
-            check() ? ( //... a diferença é que, se auth.authenticated for verdadeiro, ou seja, se tiver um use logado nos cookis, ...
-
-                <Component {... props} /> // ... ele renderiza normal
-            ) : ( // Se não, volta para a pagina inicial
-                <Redirect to={{ pathname: "/", state: { from: props.location }}} />
-            )
-        }
-    />
-);
-
-
-
-
-
 
 export default function Routes() {
+
+    const cookies = new Cookies();
+
+    const [auth, setAuth] = useState(false);
+
+    const check = () => {
+        const user = cookies.get('id');
+
+        if(user){
+            setAuth(true);
+        } else {
+            setAuth(false);
+        }
+    }
+
+    const PrivateRoute = ({ component: Component, ... rest}) => ( 
+        <Route 
+            {... rest}
+            render={props =>
+                auth ? ( 
+
+                    <Component {... props} /> 
+                ) : ( 
+                    <Redirect to={{ pathname: "/", state: { from: props.location }}} />
+                )
+            }
+        />
+    );
+    
+    useEffect(() => {
+        check();
+    }, []);
+
     return (
 
         <Router>
