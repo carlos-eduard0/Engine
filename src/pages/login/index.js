32e0bea2ff7eec4d09c381913d2bcb9e85b98981
EKaynan <erickaynan11@gmail.com>
2020-06-16 23:05:39 -0300

    finalizando reset

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -27,7 +27,7 @@ function Login() {
         const response = await api.post('/sessions', data);
 
         if(response.data.message == 'logado'){
-            await cookies.set('id', response.data, {path:'/painel'});
+            await cookies.set('id', response.data.id, {path:'/painel'});
             setTimeout(function(){ 
                 history.push('/painel'); 
             }, 2000);
