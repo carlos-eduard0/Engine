ec95b7ebb6362cab9ca959061a8a351bd093442f
EKaynan <erickaynan11@gmail.com>
2020-06-16 21:54:22 -0300

    finalizando reset

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import Cookies from 'universal-cookie'; 
-// import auth from './services/auth'; 
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'; 
 
 import Main from './pages/main';
 import Painel from './pages/painel';
@@ -10,47 +8,16 @@ import Login from './pages/login';
 import RedefinirSenha from './pages/redefinirSenha';
 import CodigoSenha from './pages/codigoSenha';
 
-
-const cookies = new Cookies();
-
-
 export default function Routes() {
-
-    const check = () => {
-        const user = cookies.get('id');
-
-        if(user){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    const PrivateRoute = ({ component: Component, ... rest}) => ( 
-        <Route 
-            {... rest}
-            render={props =>
-                check() ? ( 
-
-                    <Component {... props} /> 
-                ) : ( 
-                    <Redirect to={{ pathname: "/", state: { from: props.location }}} />
-                )
-            }
-        />
-    );
     
     return (
 
         <Router>
-            <Switch>
-                <Route path="/" exact component={Main} />
-                <PrivateRoute path="/painel" component={Painel} />
-                <Route path='/cadastro' component={Cadastro} />
-                <Route path='/login' component={Login} />
-                <Route path='/redefinir/senha' component={RedefinirSenha} />
-                <Route path='/codigo/senha/:token' component={CodigoSenha} />
-            </Switch>
+            <Route path="/" exact component={Main} />
+            <Route path='/cadastro' component={Cadastro} />
+            <Route path='/login' component={Login} />
+            <Route path='/redefinir/senha' component={RedefinirSenha} />
+            <Route path='/codigo/senha/:token' component={CodigoSenha} />
         </Router>
 
     )
